81c2b8bbe2a73fb47a85f7a50f8d25f1292dd00e
eyahya khan <eyahyakhan@eyahyas-MacBook-Pro.local>
2020-12-12 11:02:33 +0100

    add text

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,11 @@
-import styles from "./footer.module.css";
-import { CMS_USERNAME } from "../lib/constants";
+import styles from './footer.module.css';
+import { CMS_USERNAME } from '../lib/constants';
 
 export default function Footer() {
   return (
     <footer className={styles.txtfooter}>
       <>
-        <h3>@Copyright by {CMS_USERNAME}</h3>
+        <h3>@2020 Copyright by {CMS_USERNAME}</h3>
       </>
     </footer>
   );
